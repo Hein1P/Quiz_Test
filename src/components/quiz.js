95ef882618226bfbf5b5b5546e256a40083b70b3
Hein1P <heinaungsint2001@gmail.com>
2022-05-10 01:45:17 +0630

    adding alert

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,16 +1,32 @@
 import React, {useEffect} from "react";
-import getquiz from "./getquiz";
-import { Container,Box,Stack,Button,Flex,Spacer,Alert,AlertIcon } from '@chakra-ui/react'
 import axios from "axios";
+import {
+    Container,
+    Box,
+    Stack,
+    Button,
+    Flex,
+    Spacer,
+    Alert,
+    AlertIcon,
+    AlertDescription,
+    AlertTitle
+} from '@chakra-ui/react'
+
 
 const Quiz=()=>{
-
     const [state,setState] = React.useState([])
     const [count,setCount] = React.useState(0);
     const [correct,setCorrect] = React.useState(0);
     const [status,setStatus] = React.useState(null)
-    const [click,setClick] = React.useState(false)
-
+    const [click,setClick] = React.useState(false);
+    const [question,setQuestion] = React.useState('');
+    const [answer,setAnswer] = React.useState([]);
+    const [correctans,setCorrectans] = React.useState('');
+    const [alerting,setAlert] = React.useState(false)
+    function shuffle(array) {
+        return array.sort(() => Math.random() - 0.5);
+    }
     useEffect(()=>{
         const getquiz = async () => {
             const response = await axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple')
@@ -20,67 +36,68 @@ const Quiz=()=>{
         }
         getquiz();
     },[])
-
     const correction=(data)=>{
         console.log("data",data.target.value)
         setStatus(data.target.value)
         setClick(true)
-        if(data.target.value == arrindex){
+        if(data.target.value == correctans){
             setCorrect(correct+1)
         }
     }
     const next=()=>{
-        if(count < 9){
+        if(count < 10){
             setCount(count+1)
             setStatus(null)
             setClick(false)
         }
+
     }
     const prev=()=>{
         if(count > 0){
             setCount(count-1)
             setStatus(null)
         }
-
     }
-    console.log('state', state[count])
-    let answers =[]
-    let arrindex;
-    if(state.length >0){
-        let correct_ans = state[count].correct_answer
-        answers = [...state[count].incorrect_answers,correct_ans];
-        arrindex= answers.indexOf(correct_ans)
-        //console.log("answer",answers.indexOf(correct_ans))
-    }
+    useEffect(()=>{
+        if(state.length > 0 && count <10){
+            let answers =[]
+            let correct_ans = state[count].correct_answer;
+            answers = [...state[count].incorrect_answers,correct_ans];
+            setAnswer(shuffle(answers))
+            setCorrectans(correct_ans)
+            setQuestion(state[count].question)
 
+        }
+    },[count,state])
+    console.log(count)
     return(<div>
-        {state.length > 0 &&
+
+        {state.length > 0 && count <10 &&
             <Container maxW='3xl' bg='#ABD6DFFF' centerContent>
                 <span>{count+1} / 10</span>
                 <span>Score : {correct} / 10</span>
                 <Box padding='4' bg='#ABD6DFFF' color='black' maxW='md'>
-                    {state[count].question}
+                    {question}
                 </Box>
 
-                {answers.map((ans,index)=>{
+                {answer.map((ans,index)=>{
                     return <Box padding='4' bg='#ABD6DFFF' color='black' maxW='md' w={400} key={index}>
-                        {click ? <Button colorScheme='teal' variant='outline' w={400} onClick={(e)=>correction(e)} value={index} disabled>
+                        {click ? <Button colorScheme='teal' variant='outline' w={400} onClick={(e)=>correction(e)} disabled>
                             {ans}
-                        </Button>:<Button colorScheme='teal' variant='outline' w={400} onClick={(e)=>correction(e)} value={index}>
+                        </Button>:<Button colorScheme='teal' variant='outline' w={400} onClick={(e)=>correction(e)} value={ans}>
                                 {ans}
                             </Button>}
-
-                        {status == index && status == arrindex?<Alert status='success'>
+                        {status == correctans && status == ans?<Alert status='success'>
                             <AlertIcon />
                             Correct
-                        </Alert> : status == index ? <Alert status='error'>
+                        </Alert> : status == ans ? <Alert status='error'>
                             <AlertIcon />
                             Incorrect
                         </Alert>:<></>}
 
                     </Box>
-                })
-                }
+                })}
+
 
                 <Box padding='4' bg='#E7EBE0FF' color='black' maxW='md' ml='5'>
                     <Flex>
@@ -88,12 +105,34 @@ const Quiz=()=>{
                             Previous
                         </Button>
                         <Spacer width={180}/>
-                        <Button colorScheme='teal' variant='outline' width={100} onClick={next}>
+
+                        {count <9 ?<Button colorScheme='teal' variant='outline' width={100} onClick={next}>
                             Next
-                        </Button>
+                        </Button> :<Button colorScheme='teal' variant='outline' width={100} onClick={()=>setAlert(true)}>
+                            Finish
+                        </Button>}
                     </Flex>
 
                 </Box>
+                {alerting ?
+                    <Alert
+                        status='success'
+                        variant='subtle'
+                        flexDirection='column'
+                        alignItems='center'
+                        justifyContent='center'
+                        textAlign='center'
+                        height='200px'
+                    >
+                        <AlertIcon boxSize='40px' mr={0} />
+                        <AlertTitle mt={4} mb={1} fontSize='lg'>
+                            You Have Answered All Question!
+                        </AlertTitle>
+                        <AlertDescription maxWidth='sm'>
+                            You got {correct} out of 10
+                        </AlertDescription>
+                    </Alert> : <></>
+                }
             </Container>
 
         }
